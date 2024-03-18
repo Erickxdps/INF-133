4a777e08a4d991b9f34f4860e2fe253f9c38280d
Erickxdps <juliopicavia0000@gmail.com>
2024-03-18 13:57:57 -0400

    08factory y 09builder

---
+++ Semana4/09builder/client.py
@@ -0,0 +1,14 @@
+import requests
+
+url = "http://localhost:8000/pizza"
+headers = {'Content-type': 'application/json'}
+
+mi_pizza = {
+    "tamaño": "Grande",
+    "masa": "Delgada",
+    "toppings": ["Jamon", "Queso"]
+}
+response = requests.post(url, json=mi_pizza, headers=headers)
+
+print("\nNuevo cliente adicionado\n")
+print(response.json())
